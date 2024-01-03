1ac0e54bb7de20156a5b2e06254bbb9e7372b8c0
snehaathakur <snehathakur3456562@gmail.com>
2024-01-03 16:29:20 +0530

    first

---
+++ app.js
@@ -0,0 +1,51 @@
+import express from "express"
+import bodyParser from "body-parser";
+import path from "path";
+import nodemailer from "nodemailer";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename)
+
+
+const app = express();
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+var transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: "thakursnehaa007@gmail.com",
+        pass: "duan qgwq ndky saya"
+    }
+}
+)
+
+app.get("/", (req, res) => {
+    const indexPath = path.join(__dirname, "index.html");
+    console.log(indexPath)
+    res.sendFile(indexPath);
+})
+app.post("/", (req, res) => {
+    const { name, email, message } = req.body;
+    var dataToSend = `Name: ${name}\nEmail: ${email}\nMessage:${message}`;
+    const mailOptions = {
+        from: "thakursnehaa007@gmail.com",
+        to: "thakursnehaa007@gmail.com",
+        subject: "New message from Portfolio Contact Form",
+        text: dataToSend
+    }
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.error(error);
+            res.send("Error occurred. Please try again later.")
+        } else {
+            console.log("Email sent: " + info.response)
+            res.redirect("/")
+        }
+    })
+})
+app.listen(process.env.PORT || 3000, message => {
+    console.log("Server is running at port 3000");
+})
---
+++ public/asset/functions/function.js
@@ -0,0 +1,127 @@
+window.addEventListener("scroll", () => {
+  var aboutOffset = document.querySelector("#about").offsetTop;
+  var projOffset = document.querySelector(".projects").offsetTop - 10;
+  var contOffset = document.querySelector(".contact").offsetTop - 10;
+
+  var scroll = window.scrollY;
+
+  if (scroll < aboutOffset) setactive("home")
+
+  if (scroll >= aboutOffset && scroll < projOffset) setactive("about")
+
+  if (scroll >= projOffset && scroll < contOffset) setactive("proj")
+
+  if (scroll >= contOffset) setactive("cont")
+
+})
+
+function setactive(active) {
+  document.querySelector(".home").classList.remove("active")
+  document.querySelector(".about").classList.remove("active")
+  document.querySelector(".proj").classList.remove("active")
+  document.querySelector(".cont").classList.remove("active")
+  document.querySelector(`.${active}`).classList.add("active")
+}
+
+var myLink = "https://mind2023.nith.ac.in/";
+function openMind(){
+  window.open(myLink, "_blank");
+}
+
+var windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+
+var original = 38;
+var offset = 25;
+var unit = "%"
+
+var dots = document.querySelectorAll(".circle");
+var tiles = document.querySelectorAll(".tile");
+dots[0].classList.add("active-circle")
+
+// console.log(dots);
+
+dots.forEach((dot) => {
+  dot.addEventListener("click", () => {
+    i = 0;
+    for (i in dots) {
+      if (dots[i] === dot) break;
+    }
+    showSlide(i);
+  })
+})
+function updateTile(i) {
+  tiles.forEach((tile) => {
+    tile.classList.remove("active-tile")
+  })
+  tiles[i].classList.add("active-tile")
+}
+
+function showSlide(i) {
+  tileElement.setAttribute("style", `margin-left:${(original - i * offset) + unit}`)
+  updateTile(i);
+  clearInterval(id)
+  carousel();
+  updateDot(i)
+}
+var tileElement = document.querySelector(".tile");
+
+
+function updateDot(i) {
+  dots.forEach((dot) => {
+    dot.classList.remove("active-circle")
+  })
+  dots[i].classList.add("active-circle")
+}
+
+
+var i = 0;
+function right() {
+  i++;
+  if (i === 4) {
+    showSlide(0)
+    i = 0;
+  }
+  else {
+    showSlide(i);
+  }
+  updateTile(i);
+  clearInterval(id)
+  carousel();
+  updateDot(i)
+}
+
+function left() {
+  i--;
+  if (i === -1) {
+    showSlide(3)
+    i = 3;
+  }
+  else {
+    showSlide(i)
+  }
+  updateTile(i);
+  clearInterval(id)
+  carousel();
+  updateDot(i);
+}
+
+var id;
+function carousel() {
+  id = setInterval(() => {
+    right()
+  }, 4000);
+}
+
+carousel();
+
+document.querySelectorAll("span").forEach((span) => {
+  span.addEventListener("click", () => {
+    var classToAdd
+    if (span.classList.contains("left")) classToAdd = "clicked-span-left"
+    else classToAdd = "clicked-span-right"
+    span.classList.add(classToAdd)
+    setTimeout(() => {
+      span.classList.remove(classToAdd)
+    }, 200);
+  })
+})
