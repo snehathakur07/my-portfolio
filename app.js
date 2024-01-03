98321bf03bff734c8fdf316bed0b1d401386bf32
snehaathakur <snehathakur3456562@gmail.com>
2024-01-03 17:28:09 +0530

    second

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const __dirname = path.dirname(__filename)
 
 
 const app = express();
-app.use(express.static('public'))
+app.use(express.static(__dirname+'/public/'))
 app.use(bodyParser.urlencoded({ extended: true }))
 
 
